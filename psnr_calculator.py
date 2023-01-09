4ebbcb09937a2a482cf0cc0b0989d75a99f1ce89
Aashik Rasool <aashikrasool@gachon.ac.kr>
2023-01-09 15:28:27 +0900

    psnr/mse

--- psnr_calculator.py
+++ psnr_calculator.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import numpy as np
 # project_root = os.getcwd()
 # dir_dataset = os.path.join(project_root, "HR")
 # files_img = [os.path.join(dir_dataset, x) for x in os.listdir(dir_dataset)]
@@ -12,6 +13,20 @@ def psnr(img1,img2):
     psnr=print("PSNR :",psnr)
     return psnr
 
+
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
+    err=print("MSE:",err)
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
+
+
+mse(img1,img2)
 psnr(img1,img2)
 
 # print(psnr)
