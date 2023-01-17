3aef86f95a384c8e070122e0ec170d22fc7bd8d6
Aashik Rasool <aashikrasool@gachon.ac.kr>
2023-01-17 16:53:40 +0900

    conver_img added

--- actions/ActionTwo/model.py
+++ actions/ActionTwo/model.py
@@ -9,7 +9,7 @@ from PIL import Image
 import tensorflow as tf
 import tensorflow_hub as hub
 import cv2
-from convert import *
+
 
 os.environ['TFHUB_DOWNLOAD_PROGRESS'] = 'True'
 SAVED_MODEL_PATH = "https://tfhub.dev/captain-pool/esrgan-tf2/1"
@@ -34,39 +34,30 @@ def preprocess_image(image_path):
 load_image = preprocess_image(IMAGE_PATH)
 
 super_image = model(load_image)
-# super_image = tf.squeeze(super_image)
-# super_image = np.asarray(super_image)
-# super_image = tf.clip_by_value(super_image, 0, 255)
-# super_image= Image.fromarray(tf.cast(super_image, tf.uint8).numpy())
-# cv2.imwrite('action2_output/weighted3 super_image)
-
-# print(super_image.shape)
-
 
 
 
 # plot_image(tf.squeeze(super_image),'Super Resolution')
 image=np.asarray(super_image)
-image =tf.clip_by_value(image, 0, 255)
+image =tf.cast(image, tf.uint8)
 
 image=np.squeeze(image)
 
 img = Image.fromarray(np.uint8(image))
+#print(image)
 
 folder_path = "D:\\research_pycharm\\reisr\\Unsupervised-Image-Super-Resolution\\output\\action2_output"
-file_path = os.path.join(folder_path, "newo_image.png")
+file_path = os.path.join(folder_path, "neo_image.png")
 img.save(file_path)
 image = cv2.imread(file_path)
 new_size = (384, 384)
 resized_image = cv2.resize(image, new_size, interpolation = cv2.INTER_LINEAR)
 cv2.imwrite(file_path, resized_image)
-
-
-
-
-# img= np.array(img)
-cv2.imwrite(file_path,img)
-
-# file_n="/content/drive/MyDrive/Dataset/SR"
-#cv2.imwrite('/content/drive/MyDrive/Dataset/SR',image)
-# # image = Image.fromarray(tf.cast(image, tf.uint8).numpy())
+#
+# image = Image.fromarray(img)
+# image.save(file_path)
+# #
+#
+#
+#
+# cv2.imwrite(file_path,img)
